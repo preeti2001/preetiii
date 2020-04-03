f0e39383f142776e9d9b5dd13bf1135dff41f5e9
preeti2001 <55980106+preeti2001@users.noreply.github.com>
2020-04-03 11:32:58 +0530

    Add files via upload

---
+++ If else/even odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("Enter a number");
+    scanf("%d",&x);
+    if(x%2==0)
+    printf("the number is even");
+    else
+    printf("the number is odd");
+}
