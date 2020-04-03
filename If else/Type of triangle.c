a7e1a9efaa7c8dc97a2e136dfd400d627291cc88
preeti2001 <55980106+preeti2001@users.noreply.github.com>
2020-04-03 13:55:40 +0530

    Add files via upload

---
+++ If else/Type of triangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,c,a1,b1,c1;
+ printf("\nEnter 3 side of the triangle  :");
+ scanf("%d %d %d",&a,&b,&c);
+ printf("\nEnter 3 angles of the triangle  :");
+ scanf("%d %d %d",&a1,&b1,&c1);
+ if ( (a+b>c) && (a+c>b) && (c+b>a) )
+   {
+    if(a==b && b==c && a1==b1 && b1==c1)
+      {
+       printf("\nTringle formed is an Equilateral triangle");
+      }
+    else if(a==b && b!=c)
+      {
+       printf("\nTrinangle in Isoceles triangle");
+      }
+    else if (a!=b && b!=c && a1!=b1 && b1!=c1)
+      {
+       printf("\nTriangle is a Scalene triangle");
+      }
+   }
+ else
+   {
+    printf("\nTriangle is not valid");
+   }
+}
