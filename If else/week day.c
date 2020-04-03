daa0280b0fa7fb13e148212332ce55ec1aef61f0
preeti2001 <55980106+preeti2001@users.noreply.github.com>
2020-04-03 12:31:52 +0530

    Add files via upload

---
+++ If else/week day.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+ char a;
+ printf("\nEnter an character :");
+ scanf("%c",&a);
+ switch(a)
+  {
+   case '1' : printf("\nSunday");
+	      break;
+   case '2' : printf("\nMonday");
+	      break;
+   case '3' : printf("\nTuesday");
+	      break;
+   case '4' : printf("\nWednesday");
+	      break;
+   case '5' : printf("\nThursday");
+	      break;
+   case '6' : printf("\nFriday");
+	      break;
+   case '7' : printf("\nSaturday");
+	      break;
+   default  : break;
+  }
+}
